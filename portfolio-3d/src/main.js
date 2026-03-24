f23e37abbd205dbcd6568150857faa1bdadd4ad9
mac <chibinoo.tv@gmail.com>
2026-03-24 22:18:50 +0100

    adding movement

--- portfolio-3d/src/main.js
+++ portfolio-3d/src/main.js
@@ -1,11 +1,38 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+import gsap from "gsap";
 
 console.log("JS is running");
 
+//scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x222222);
 
+//movement
+const points = [
+  new THREE.Vector3(0,1.6,5),
+  new THREE.Vector3(5,1.6,0),
+  new THREE.Vector3(-5,1.6,0),
+  new THREE.Vector3(0,1.6,-5)
+];
+
+let currentPoint=0;
+
+//keyboard 
+document.addEventListener("keydown", (e)=>{
+  console.log("Key pressed:", e.key);
+
+  if(e.key==="ArrowRight"){
+    moveTo((currentPoint+1)%points.length);
+  }
+
+  if(e.key==="ArrowLeft"){
+    moveTo((currentPoint-1+points.length)%points.length);
+  }
+})
+
+//camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -15,13 +42,17 @@ const camera = new THREE.PerspectiveCamera(
 
 camera.position.set(0,1.6,8);
 
-const renderer = new THREE.WebGLRenderer();
+//renderer
+const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
+//light
 const light=new THREE.AmbientLight(0xffffff, 1.5);
 scene.add(light);
 
+//load model
 const loader=new GLTFLoader();
 loader.load('/ruins.glb',(gltf)=>{
   const model=gltf.scene;
@@ -37,6 +68,26 @@ undefined,
 }
 );
 
+//controls
+const controls = new PointerLockControls(camera, document.body);
+
+document.addEventListener('click', () => {
+  controls.lock();
+});
+
+function moveTo(index){
+  console.log("moving to:", index);
+
+  const target=points[index];
+
+  gsap.to(camera.position, {
+    duration:1,
+    x: target.x,
+    y: target.y,
+    z: target.z,
+  });
+  currentPoint=index;
+}
 
 function animate() {
   requestAnimationFrame(animate);
